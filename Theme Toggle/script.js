1747548792c29415a78b33bbec2bb36661d4e5dc
Devarshi Boghani <boghanidevarshi@gmail.com>
2026-05-06 09:34:43 +0530

    Theme Toggle

---
+++ Theme Toggle/script.js
@@ -0,0 +1,61 @@
+const toggle = document.getElementById("toggle");
+const lblLight = document.getElementById("lbl-light"); 
+const lblDark = document.getElementById("lbl-dark");
+
+const heading = document.getElementById("heading");
+const icon = document.getElementById("icon");
+const subtitle = document.getElementById("subtitle");
+
+// Load theme
+const theme = localStorage.getItem("theme");
+
+if (theme === "dark") {
+    document.body.classList.add("dark");
+    toggle.checked = true;
+
+    heading.innerText = "Good Night!";
+    icon.innerText = "🌙";
+    subtitle.innerText = "Dark Theme is active - easy on the eyes."
+
+    lblLight.classList.remove("active");
+    lblDark.classList.add("active");
+
+} else {
+    document.body.classList.remove("dark");
+    toggle.checked = false;
+
+    heading.innerText = "Good Morning!";
+    icon.innerText = "☀️";
+    subtitle.innerText = "Light Theme is active - bright and clear."
+
+    lblDark.classList.remove("active");
+    lblLight.classList.add("active");
+}
+
+// Toggle
+toggle.onchange = () => {
+
+    if (toggle.checked) {
+
+        document.body.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+
+        heading.innerText = "Good Night!"
+        icon.innerText = "🌙";
+        subtitle.innerText = "Dark Theme is active - easy on the eyes."
+
+        lblLight.classList.remove("active");
+        lblLight.classList.add("active");
+
+    } else {
+        document.body.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+
+        heading.innerText = "Good Morning!"
+        icon.innerText = "☀️";
+        subtitle.innerText = "Light Theme is active - bright and clear."
+
+        lblDark.classList.remove("active");
+        lblLight.classList.add("active");
+    }
+}
